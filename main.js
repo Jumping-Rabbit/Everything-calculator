85eb6a52e0c627cb25fae7850f321cea0376cf35
Ryan Liu <ryanliu25@gmail.com>
2026-05-24 00:19:10 -0700

    uhh idk

---
+++ main.js
@@ -0,0 +1,9 @@
+const ButtonEnum = Object.freeze({
+    MATH: { label: "Math", url: "math.html"},
+    CHEMISTRY: { label: "Chemistry", url: "chemistry.html"},
+    PHYSICS: { label: "Physics", url: "physics.html"}
+});
+
+for (const button in ButtonEnum){
+    
+}
